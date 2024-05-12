99e9977e93ea1df65c27d7a790a0378c212d3426
Ukandu Giddel <giddeluk@gmail.com>
2024-05-11 17:29:43 -0700

    Implemented the Logic to get the COmputer's Choice

---
+++ index.js
@@ -0,0 +1,15 @@
+function getComputerChoice(){
+    // Generate Random Number
+    let randomNumber = Math.floor(Math.random() * (3 - 1 + 1)) + 1;
+    // Return either Rock, Paper or Scissors According to the Random Number
+    switch(randomNumber){
+        case 1:
+            return "rock";
+            break;
+        case 2:
+            return "paper";
+            break;
+        case 3:
+            return "scissors";
+    }
+}
