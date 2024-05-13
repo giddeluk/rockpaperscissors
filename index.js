b03d2f5a572fbc3e9503930594b18cf8f0941a35
Ukandu Giddel <giddeluk@gmail.com>
2024-05-13 14:15:49 -0700

    Implemeted the Logic to Play One Round

--- index.js
+++ index.js
@@ -22,3 +22,47 @@ function getHumanChoice(){
 let humanScore = 0;
 let computerScore = 0;
 
+// Play One Round
+function playRound(humanChoice, computerChoice){
+    // Decide the Winner
+    let roundWinner = () => {
+        if (humanChoice === computerChoice) {
+            console.log(`It's a tie, ${computerChoice} draws ${humanChoice}`);
+        }
+        else if (humanChoice === "ROCK" && computerChoice === "PAPER") {
+            console.log(`You Lose! (${computerChoice} wins ${humanChoice})`);
+            computerScore = computerScore + 1;
+        }
+        else if (humanChoice === "ROCK" && computerChoice === "SCISSORS") {
+            console.log(`You Win! (${humanChoice} wins ${computerChoice})`);
+            humanScore = humanScore + 1;
+        }
+        else if (humanChoice === "PAPER" && computerChoice === "ROCK") {
+            console.log(`You Win! (${humanChoice} wins ${computerChoice})`);
+            humanScore = humanScore + 1;
+        }
+        else if (humanChoice === "PAPER" && computerChoice === "SCISSORS") {
+            console.log(`You Lose(${computerChoice} wins ${humanChoice})`);
+            computerScore = computerScore + 1;
+        }
+        else if (humanChoice === "SCISSORS" && computerChoice === "ROCK") {
+            console.log(`You Lose! (${computerChoice} wins ${humanChoice})`);
+            computerScore = computerScore + 1;
+        }
+        else if (humanChoice === "SCISSORS" && computerChoice === "PAPER") {
+            console.log(`You Win! (${humanChoice} wins ${computerChoice})`);
+            humanScore = humanScore + 1;
+        }
+        else {
+            console.log(`Please Enter Either "ROCK", "PAPER" or "SCISSORS"`);
+        }
+    };
+}
+
+// Selections
+const humanSelection = getHumanChoice().toUpperCase();
+const computerSelection = getComputerChoice().toUpperCase();
+
+// Execute the Play Round Function
+playRound(humanSelection, computerSelection);
+
